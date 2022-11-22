11f3d5a37a5ff921273587f3a4fa64faf3550218
Rajan Kesharwani <rajan.kesharwani@rationalfx.com>
2022-11-22 16:32:05 +0530

    bankaccount

---
+++ BANKACCOUNTMGT/src/main/java/com/work4you/bankaccountmgt/BankaccountmgtApplication.java
@@ -0,0 +1,13 @@
+package com.work4you.bankaccountmgt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankaccountmgtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankaccountmgtApplication.class, args);
+	}
+
+}
---
+++ BANKACCOUNTMGT/src/test/java/com/work4you/bankaccountmgt/BankaccountmgtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.work4you.bankaccountmgt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankaccountmgtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
